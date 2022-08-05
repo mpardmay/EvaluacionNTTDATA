9e4ce97fb5a912cf1c2aa302f5ce22777ee175b4
mpardmay <mpardmay@emeal.nttdata.com>
2022-08-05 17:26:18 -0500

    unificacion de ambas branch en una

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,10 @@ public class Main {
         cuentaNuevaEnMain.retirar(60000);
         //esta es una modificacion del ejercicio de main
         cuentaNuevaEnMain.retirar(1000);
+
+        //este ejercicio hace parte de la rama dev
+        Cuenta cuentaNueva = new Cuenta("prueba");
+        cuentaNueva.ingresar(600000);
+        cuentaNueva.retirar(60000);
     }
 }
