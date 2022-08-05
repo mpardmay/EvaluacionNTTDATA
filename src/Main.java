516ca75931bbd6a1230d6b651f1250b580e3fb75
mpardmay <mpardmay@emeal.nttdata.com>
2022-08-05 16:48:15 -0500

    adicion a branch "main"

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ public class Main {
     public static void main(String[] args){
         Cuenta miCuenta = new Cuenta("michael");
         miCuenta.ingresar(20000);
-        System.out.println(miCuenta.getCantidad());
         miCuenta.retirar(60000);
-        miCuenta.retirar(0);
 
-        //este ejercicio hace parte de la rama dev
-        Cuenta cuentaNueva = new Cuenta("prueba");
-        cuentaNueva.ingresar(600000);
-        cuentaNueva.retirar(60000);
+
+        //este ejercicio hace parte de la rama main
+        Cuenta cuentaNuevaEnMain = new Cuenta("main");
+        cuentaNuevaEnMain.ingresar(2000);
+        cuentaNuevaEnMain.retirar(60000);
     }
 }
